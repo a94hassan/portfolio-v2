d21815a809963aa8c0920894b1e555551fce7392
Hassan Ammar <h.ammar@hotmail.de>
2024-03-27 12:17:28 +0100

    header, footer, above the fold section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
